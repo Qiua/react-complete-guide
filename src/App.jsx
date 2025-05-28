ef06b32166db40016f4f12b87c71374743982fc8
Andrey Aires <andrey.aires@gmail.com>
2025-05-28 15:28:21 -0300

    c2-Dynamic Values

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+function getRandomDescription() {
+  const reactDescriptions = ["Fundamental", "Essential", "Core", "Master"];
+  const randomIndex = Math.floor(Math.random() * reactDescriptions.length);
+  return reactDescriptions[randomIndex];
+}
+
 function Header() {
+  const description = getRandomDescription();
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
