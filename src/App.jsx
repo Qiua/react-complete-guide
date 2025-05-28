f70258524d195535c649b30d39678d63ecddf16f
Andrey Aires <andrey.aires@gmail.com>
2025-05-28 16:33:38 -0300

    c4-Project Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,7 @@
-import logo from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data";
 
-function getRandomDescription() {
-    const reactDescriptions = ["Fundamental", "Essential", "Core", "Master"];
-    const randomIndex = Math.floor(Math.random() * reactDescriptions.length);
-    return reactDescriptions[randomIndex];
-}
-
-function Header() {
-    const description = getRandomDescription();
-    return (
-        <header>
-            <img src={logo} alt="Stylized atom" />
-            <h1>React Essentials</h1>
-            <p>
-                {description} React concepts you will need for almost any app
-                you are going to build!
-            </p>
-        </header>
-    );
-}
-
-function CoreConcepts({ img, title = "Title", description }) {
-    // Default title to "Title" if not provided
-    // Destructuring props to extract img, title, and description
-    return (
-        <li>
-            <img src={img} alt={description}></img>
-            <h3>{title}</h3>
-            <p>{description}</p>
-        </li>
-    );
-}
+import Header from "./components/Header";
+import CoreConcepts from "./components/CoreConcepts";
 
 function App() {
     return (
---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,11 @@
+export default function CoreConcepts({ img, title = "Title", description }) {
+    // Default title to "Title" if not provided
+    // Destructuring props to extract img, title, and description
+    return (
+        <li>
+            <img src={img} alt={description}></img>
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </li>
+    );
+}
