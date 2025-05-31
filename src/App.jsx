1d0490c3ba99ea929de9840cbacfa07487fcec48
Andrey Aires <andrey.aires@gmail.com>
2025-05-31 16:53:06 -0300

    6.4-Rendering Arrays of content

--- src/App.jsx
+++ src/App.jsx
@@ -38,12 +38,11 @@ function App() {
       <Header /> {/* First component! */}
       <main>
         <section id='core-concepts'>
-          <h2>Time to get started!</h2>
+          <h2>React Core Concepts</h2>
           <ul>
-            <CoreConcepts {...CORE_CONCEPTS[0]} />
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
-            <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((concept, index) => (
+              <CoreConcepts key={index} {...concept} />
+            ))}
           </ul>
         </section>
         <section id='examples'>
