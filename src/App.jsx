7f0a7ffbd6fae0298610ed7075ae2d4e09fb3dd3
Andrey Aires <andrey.aires@gmail.com>
2025-06-02 17:57:56 -0300

    c2-Basic Content

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 function App() {
-  return <p>Comming Soon...</p>
+  return (
+    <main>
+      <div id='game-container'>
+        <ol id='players'>
+          <li>
+            <span className='player-name'>Player 1</span>
+            <span className='player-symbol'>X</span>
+            <button>Edit</button>
+          </li>
+          <li>
+            <span className='player-name'>Player 2</span>
+            <span className='player-symbol'>O</span>
+            <button>Edit</button>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+      GAME LOG
+    </main>
+  )
 }
 
 export default App
